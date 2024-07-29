c11d50e788660f2498028949d1297202dcaaea2b
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-07-29 08:57:02 +0200

    card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -4,24 +4,30 @@ import axios from 'axios';
 import './pokemonCard.css';
 
 const PokemonCard = ({pokemon}) => {
-    const [thisPokemon, setThisPokemon] = useState({});
-    console.log(thisPokemon);
+    const [thisPokemon, setThisPokemon] = useState(null);
     
     useEffect(() => {
         getPokemon();
     }, [])
-
+    
     async function getPokemon() {
         const response = await axios.get(pokemon.url);
         setThisPokemon(response.data);
     }
+    console.log(thisPokemon);
+
+    if(!thisPokemon) {
+        return <div>Loading...</div>
+    }
 
     return (
-            <div className='container'>
-                <img src="" alt="" />
+            <div className='container' id={thisPokemon.types[0]?.type.name}>
+                <img src={thisPokemon.sprites.front_default} alt={pokemon.name} />
                 <h4> {pokemon.name}</h4>
                 <p>#{thisPokemon.id < 10 ? '00' : '0'}{thisPokemon.id}</p>
-                <p>Fire</p>
+                <p>
+                    {thisPokemon.types[0]?.type.name}
+                </p>
             </div>
     )
 }
