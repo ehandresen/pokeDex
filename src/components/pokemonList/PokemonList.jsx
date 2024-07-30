a6f16724ae62658f5efa3b3f20669ffeef8533b4
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-07-30 17:30:35 +0200

    fetch all 151 pokemons

--- src/components/pokemonList/PokemonList.jsx
+++ src/components/pokemonList/PokemonList.jsx
@@ -5,17 +5,33 @@ import { useEffect, useState } from 'react';
 
 const PokemonList = () => {
     const [pokemonList, setPokemonList] = useState([]);
+    const [nextUrl, setNextUrl] = useState('https://pokeapi.co/api/v2/pokemon');
 
     useEffect(()=>{
         getPokemons();
 
-    }, [])
+    }, [pokemonList])
 
     const getPokemons = async () => {
 
-        const pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
-        console.log(pokemons.data.results);
-        setPokemonList(pokemons.data.results)
+        if (!nextUrl || pokemonList.length > 150) {
+            return 
+        }
+
+        console.log(nextUrl)
+        if (nextUrl === 'https://pokeapi.co/api/v2/pokemon?offset=140&limit=20') {
+            const lastUrl = 'https://pokeapi.co/api/v2/pokemon?offset=140&limit=11';
+            const response = await axios.get(lastUrl);
+            const pokemons = response.data.results;
+            setPokemonList((prevList) => [...prevList, ...pokemons]);
+            setNextUrl(response.data.next)
+            return;
+        }
+
+        const response = await axios.get(nextUrl);
+        const pokemons = response.data.results;
+        setPokemonList((prevList) => [...prevList, ...pokemons]);
+        setNextUrl(response.data.next)
 
         // const list = [];
         // for (let i = 1; i <= 151; i++) {
@@ -28,9 +44,10 @@ const PokemonList = () => {
     }
 
     return (
-        <div className='container'>{pokemonList.map((pokemon, i) => (
-            <PokemonCard key={i} pokemon={pokemon} />)
-        )}
+        <div className='container'>
+            {pokemonList.map((pokemon, i) => (
+                <PokemonCard key={i} pokemon={pokemon} />)
+            )}
         </div>
     )
 }
