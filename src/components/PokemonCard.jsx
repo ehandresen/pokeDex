95c0d4095bb0769a1cff8c9da26e917875e43979
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-07-30 18:08:12 +0200

    fix id and type printed

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -20,15 +20,18 @@ const PokemonCard = ({pokemon}) => {
         return <div>Loading...</div>
     }
 
+    // Format the Pokémon ID with leading zeros
+    const formattedId = thisPokemon.id.toString().padStart(3, '0');
+
     return (
             <div className='card' id={thisPokemon.types[0]?.type.name}>
                 <div className="image-container">
                     <img src={thisPokemon.sprites.front_default} alt={pokemon.name} />
                 </div>
                 <h4> {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h4>
-                <p id='id'>#{thisPokemon.id < 10 ? '00' : '0'}{thisPokemon.id}</p>
+                <p id='id'>#{formattedId}</p>
                 <p id='type' className={thisPokemon.types[0]?.type.name}>
-                    {thisPokemon.types[0]?.type.name}
+                    {thisPokemon.types[0]?.type.name.charAt(0).toUpperCase()}{thisPokemon.types[0]?.type.name.slice(1)}
                 </p>
             </div>
     )
